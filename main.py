a14773480e757531e5398bea820ed88440c5e9ca
Shishkin PRO <viktorshish@gmail.com>
2024-02-27 15:54:18 +0300

    Download book

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import requests
 
 
-def download_img():
-    url = "https://dvmn.org/filer/canonical/1542890876/16/"
+def download_img(url):
 
     response = requests.get(url)
     response.raise_for_status()
 
-    file_name = 'dvmn.svg'
+    file_name = 'book.txt'
     with open(file_name, 'wb') as file:
         file.write(response.content)
 
 
+def main():
+    url = 'https://tululu.org/txt.php?id=32168'
+
+    download_img(url)
+
+
 if __name__ == '__main__':
-    download_img()
+    main()
