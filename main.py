1d1b957b5c63d988ee85a2e2abd739889710bf38
Shishkin PRO <viktorshish@gmail.com>
2024-02-28 00:47:49 +0300

    Check redirect

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import os
 import requests
 
 
-def download_img(url, path_name):
-
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open(path_name, 'wb') as file:
-        file.write(response.content)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
 
 
 def main():
     os.makedirs('books', exist_ok=True)
 
-    start_id = 3268
+    start_id = 1
     books_amount = 10
     for book_id in range(start_id, start_id + books_amount):
-        url = f'https://tululu.org/txt.php?id={book_id}'
+        url = f'https://tululu.org/txt.php'
+        params = {"id": book_id}
         book_path = os.path.join('books', f'book {book_id}.txt')
-        download_img(url, book_path)
+
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            check_for_redirect(response)
+
+            with open(book_path, 'wb') as file:
+                file.write(response.content)
+
+        except requests.exceptions.HTTPError:
+            print(f'Не удалось загрузить книгу с ID {book_id}:')
 
 
 if __name__ == '__main__':
